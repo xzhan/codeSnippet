e6cae5ea160c68c30e1fb5a5715ef77a78b8d5cb
Xuguang Zhan (xzhan) <xzhan@cisco.com>
2020-12-24 01:28:31 +0000

    ratelimiter commit
    ratelimiter commit

---
+++ kafkaStreamDemon/src/test/java/com/cisco/webex/notification/kafkaStreamDemon/KafkaStreamDemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cisco.webex.notification.kafkaStreamDemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamDemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
